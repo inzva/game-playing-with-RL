8056421c06698babce0f12fd3545e8d749547bb3
herkut <erkuthamdi@gmail.com>
2019-04-24 22:39:06 +0300

    Insert models solving mountain car continuous

---
+++ ddpg/bipedal_walker_continuous/critic_network.py
@@ -0,0 +1,58 @@
+from keras import layers, models, optimizers
+from keras import backend as K
+
+
+class CriticNetwork:
+    """Critic (Value) Model."""
+
+    def __init__(self, state_size, action_size):
+        """
+        Initialize parameters and build model.
+
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+
+        # Initialize any other variables here
+
+        self.build_model()
+
+    def build_model(self):
+        """
+        Build a critic (value) network that maps
+        (state, action) pairs -> Q-values.
+        """
+        # Define input layers
+        states = layers.Input(shape=(self.state_size,), name='states')
+        actions = layers.Input(shape=(self.action_size,), name='actions')
+
+        # Add hidden layer(s) for state pathway
+        net = layers.Dense(units=20, activation='relu')(states)
+        net = layers.Add()([net, actions])
+        net = layers.Dense(units=20, activation='relu')(net)
+
+        lin_states = layers.Dense(units=20, activation='relu')(states)
+
+        net = layers.Add()([net, lin_states])
+
+        # Add final output layer to prduce action values (Q values)
+        Q_values = layers.Dense(units=1, name='q_values')(net)
+
+        # Create Keras model
+        self.model = models.Model(inputs=[states, actions], outputs=Q_values)
+
+        # Define optimizer and compile model for training with
+        # built-in loss function
+        optimizer = optimizers.Adam(lr=0.05)
+        self.model.compile(optimizer=optimizer, loss='mse')
+
+        # Compute action gradients (derivative of Q values w.r.t. to actions)
+        action_gradients = K.gradients(Q_values, actions)
+
+        # Define an additional function to fetch action gradients (to be
+        # used by actor model)
+        self.get_action_gradients = K.function(inputs=[*self.model.input, K.learning_phase()], outputs=action_gradients)
